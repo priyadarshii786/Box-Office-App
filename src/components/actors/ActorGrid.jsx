0910815c7def60f4cba34e274a4b750f2000bcac
priyadarshii786 <tathagatp918@gmail.com>
2023-10-20 01:18:32 +0530

    Maroon

---
+++ src/components/actors/ActorGrid.jsx
@@ -0,0 +1,25 @@
+import { FlexGrid } from "../common/FlexGrid";
+import ActorCard from "./ActorCard";
+import NotFoundImgSrc from '../../lib/Not-found-image.png';
+
+const ActorGrid = ({ actors }) => {
+    return (
+        <FlexGrid>{
+            actors.map(data => (
+                <ActorCard
+                  key={data.person.id}
+                  name={data.person.name}
+                  country={data.person.country ? data.person.country.name : null}
+                  birthday={data.person.birthday}
+                  deathday={data.person.deathday}
+                  gender={data.person.gender}
+                  image={data.person.image ? data.person.image.medium : NotFoundImgSrc}
+                  
+
+                />
+            ))}
+        </FlexGrid>
+    );
+};
+
+export default ActorGrid;
